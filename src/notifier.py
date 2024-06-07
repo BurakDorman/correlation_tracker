d400bae14d74dc8ccda1a294dcfe4a0d4267b513
Burak <47524842+BurakDorman@users.noreply.github.com>
2024-06-07 03:59:58 +0300

    Update and rename notifier.py to src/notifier.py

---
+++ src/notifier.py
@@ -0,0 +1,17 @@
+# This code block is required to send messages to the Telegram chat(s).
+# It sends a message as the program starts, or when it catches a correlation.
+import sys
+import os
+import asyncio
+from telegram import Bot
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'util'))
+from config import BOT_TOKEN, CHAT_ID
+
+bot = Bot(token=BOT_TOKEN)
+
+async def send_notification(message, chat_id=CHAT_ID):
+    try:
+        await asyncio.wait_for(bot.send_message(chat_id=chat_id, text=message), timeout=10)
+    except asyncio.TimeoutError:
+        print("Notification sending timed out")
