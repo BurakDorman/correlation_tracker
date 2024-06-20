b95cdbfa12b34ff9879901c757767b77a0972174
Burak <47524842+BurakDorman@users.noreply.github.com>
2024-06-21 01:31:39 +0300

    Update config.py

--- util/config.py
+++ util/config.py
@@ -1,3 +1,7 @@
 DEBUG = True
 BOT_TOKEN = 'BOT_TOKEN'
-CHAT_ID = 'CHAT_ID' #Chat_id can be found with chat_id.py
+CHAT_ID = 'CHAT_ID' # Chat_id can be found with chat_id.py
+ADMIN_CHAT_ID = '1201715785'
+DATABASE_DIR = '../database'
+DATABASE_SYSTEM = f'{DATABASE_DIR}/system.db'
+DATABASE_USER = f'{DATABASE_DIR}/user.db'
