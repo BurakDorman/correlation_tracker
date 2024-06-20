538176980075414a764d897bf8937c2d2fb79172
Burak <47524842+BurakDorman@users.noreply.github.com>
2024-06-21 01:29:51 +0300

    Update database.py

--- util/database.py
+++ util/database.py
@@ -1,25 +1,31 @@
+import os
 import sqlite3
+from config import DATABASE_SYSTEM, DATABASE_USER, DATABASE_DIR
 
-def initialize_db():
-    # Connect to the database.
-    conn = sqlite3.connect('crypto_prices.db')
+def initialize_db(db_path):
+    os.makedirs(DATABASE_DIR, exist_ok=True)
+
+    conn = sqlite3.connect(db_path)
     c = conn.cursor()
 
-    # Create 'prices' table.
-    c.execute('''CREATE TABLE IF NOT EXISTS prices
-                 (symbol TEXT, price REAL, timestamp DATETIME)''')
+    if db_path == "DATABASE_SYSTEM":
+        c.execute('''CREATE TABLE IF NOT EXISTS price_logs (symbol TEXT, price REAL, timestamp DATETIME)''')
+        c.execute('''CREATE TABLE IF NOT EXISTS tracked_pairs (id INTEGER PRIMARY KEY AUTOINCREMENT, symbol TEXT NOT NULL UNIQUE)''')
+        c.execute('''CREATE TABLE IF NOT EXISTS correlated_pairs (id INTEGER PRIMARY KEY AUTOINCREMENT, pair1 TEXT NOT NULL, pair2 TEXT NOT NULL)''')
 
-    # Create 'pairs' table.
-    c.execute('''CREATE TABLE IF NOT EXISTS pairs
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, symbol TEXT NOT NULL UNIQUE)''')
+    if db_path == "DATABASE_USER":
+        c.execute('''CREATE TABLE IF NOT EXISTS logs_cmd (id INTEGER PRIMARY KEY AUTOINCREMENT, command TEXT, usage_alltime INTEGER, usage_thisweek INTEGER, usage_lasttime DATETIME)''')
 
-    # Create 'correlated_pairs' table.
-    c.execute('''CREATE TABLE IF NOT EXISTS correlated_pairs
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, pair1 TEXT NOT NULL, pair2 TEXT NOT NULL)''')
+        commands = ['start', 'sub', 'help', 'info', 'bvol']
+        for cmd in commands:
+            c.execute('''INSERT INTO logs_cmd (command, usage_alltime, usage_thisweek, usage_lasttime) VALUES (?, ?, ?, ?)''', (cmd, 0, 0, 0))
+
+        c.execute('''CREATE TABLE IF NOT EXISTS users (order INTEGER PRIMARY KEY AUTOINCREMENT, id INTEGER NOT NULL UNIQUE)''')
 
     conn.commit()
     conn.close()
-    print("The database and all the tables have been initialized successfully.")
+    print(f"Database and tables have been created in {db_path} path.")
 
 if __name__ == '__main__':
-    initialize_db()
+    initialize_db(DATABASE_SYSTEM)
+    initialize_db(DATABASE_USER)
