757480ce58f4ee6c8487053d6856151fe5623a08
Burak <47524842+BurakDorman@users.noreply.github.com>
2024-06-21 03:13:57 +0300

    Update notifier.py

--- src/notifier.py
+++ src/notifier.py
@@ -3,15 +3,15 @@
 import sys
 import os
 import asyncio
-from telegram import Bot
+from telegram import Bot # type: ignore
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'util'))
-from config import BOT_TOKEN, CHAT_ID
+from config import BOT_TOKEN, GROUP_ID
 
 bot = Bot(token=BOT_TOKEN)
 
-async def send_notification(message, chat_id=CHAT_ID):
+async def send_notification(message, chat_id=GROUP_ID):
     try:
         await asyncio.wait_for(bot.send_message(chat_id=chat_id, text=message), timeout=10)
     except asyncio.TimeoutError:
-        print("Notification sending timed out")
+        print("Notification sending timed out.")
