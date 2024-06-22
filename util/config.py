0f2e55df69167e20288d8f0994275ba95882a9e8
Burak <47524842+BurakDorman@users.noreply.github.com>
2024-06-22 23:25:14 +0300

    Update config.py

--- util/config.py
+++ util/config.py
@@ -1,7 +1,11 @@
-DEBUG = True
+import os
+
 BOT_TOKEN = 'BOT_TOKEN'
-CHAT_ID = 'CHAT_ID' # Chat_id can be found with chat_id.py
-ADMIN_CHAT_ID = '1201715785'
-DATABASE_DIR = '../database'
-DATABASE_SYSTEM = f'{DATABASE_DIR}/system.db'
-DATABASE_USER = f'{DATABASE_DIR}/user.db'
+GROUP_ID = 'CHAT_ID'
+ADMIN_ID = 'ADMIN_ID' # {'id': 7475046663, 'is_bot': False, 'first_name': 'Burak', 'last_name': 'Dorman', 'username': 'burakdorman', 'language_code': 'en'}
+DATABASE_DIR = os.path.join(os.path.dirname(__file__), '../database/')
+SYSTEM_DATABASE= os.path.join(DATABASE_DIR, 'system.db')
+USER_DATABASE = os.path.join(DATABASE_DIR, 'user.db')
+LANGUAGE_FILE = os.path.join(DATABASE_DIR, 'strings.json')
+
+DEBUG = True
